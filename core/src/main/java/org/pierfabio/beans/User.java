8f804924632d8460de759c80a42d5cae5b624390
Pierfabio <pierfabiosanna7@gmail.com>
2020-04-09 00:03:07 +0200

    update User bean with lombok annotations

--- core/src/main/java/org/pierfabio/beans/User.java
+++ core/src/main/java/org/pierfabio/beans/User.java
@@ -1,5 +1,12 @@
 package org.pierfabio.beans;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     private String id;
