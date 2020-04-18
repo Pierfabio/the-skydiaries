bfa467be1641f6c6f222f479b0d60471087730e4
Pierfabio <sannapierfabio7@gmail.com>
2020-04-18 12:50:31 +0200

    Added Components and Routing

---
+++ src/main/ui/skydiaries/src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const About = () => (
+        <div>
+            <h2>About Page</h2>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        </div>
+
+    )
---
+++ src/main/ui/skydiaries/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Home = () => (
+        <div>
+            <h2>Hello World!</h2>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        </div>
+
+    )
