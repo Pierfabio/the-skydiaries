418a610080f4ae284af70a39318ad532c1f2f524
Pierfabio <sannapierfabio7@gmail.com>
2020-04-20 00:08:16 +0200

    Add Navbar

--- src/main/ui/skydiaries/src/App.js
+++ src/main/ui/skydiaries/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
-import { Home } from './components/Home';
-import { About } from './components/About';
-import { Contact } from './components/Contact';
-import { NoMatch } from './components/NoMatch';
-import { Layout } from './layouts/Layout';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+import { NoMatch } from './pages/NoMatch';
+import { Layout } from './components/Layout';
+import { NavigationBar } from './components/NavigationBar'
 
 function App() {
   return (
    <React.Fragment>
-    <Layout>
-      <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route component={NoMatch} />          
-          </Switch>
-      </Router>
-    </Layout>
+     <NavigationBar/>
+      <Layout>
+        <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route component={NoMatch} />          
+            </Switch>
+        </Router>
+      </Layout>
    </React.Fragment>
   );
 }
